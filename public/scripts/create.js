f5f25d78a3cff1934a3c4a847ab5378c023ddaab
Nathan Edlen <nathan.edlen@gmail.com>
2017-03-20 15:45:16 -0600

    create page tab fucntionality skeleton

--- public/scripts/create.js
+++ public/scripts/create.js
@@ -1,35 +1,57 @@
 $(document).ready(() => {
 
+  createTimesForm()
+  createPeopleForm()
+  createDescriptionForm()
+
+  $('.desc').hide()
+  $('.invitee').hide()
+
   $('.datepicker').pickadate({
-   selectMonths: true, // Creates a dropdown to control month
-   selectYears: 15 // Creates a dropdown of 15 years to control year
- });
-
- var slider = document.getElementById('test5');
-  noUiSlider.create(slider, {
-   start: [20, 80],
-   connect: true,
-   step: 1,
-   range: {
-     'min': 0,
-     'max': 100
-   },
-   format: wNumb({
-     decimals: 0
-   })
+     selectMonths: true,
+     selectYears: 15
   });
 
-var slider2 = document.getElementById('test5');
-  noUiSlider.create(slider2, {
-   start: [20, 80],
-   connect: true,
-   step: 1,
-   range: {
-     'min': 0,
-     'max': 100
-   },
-   format: wNumb({
-     decimals: 0
-   })
-  });
+  $('#start').timepicker();
+  $('#end').timepicker();
+
+  function createTimesForm() {
+    $('body').addClass('containCenterColumn')
+    $('body').append($('<div>', {id: 'createPageMain'}))
+    $('body').append($('<div>', {class: 'createInputs containCenterColumn', id: 'inputForm'}))
+    $('#createPageMain').append($('<button>', {name: 'createTimesTab', id: 'timesToggle'}).text('Times'))
+    $('#createPageMain').append($('<button>', {name: 'createInvitesTab', id: 'invitesToggle'}).text('People'))
+    $('#createPageMain').append($('<button>', {name: 'createDescriptionTab', id: 'descriptionToggle'}).text('Description'))
+    $('#inputForm').append($('<input>', {type: 'date', class: 'datepicker', placeholder: 'Please choose a Date', class: 'times'}))
+    $('#inputForm').append($('<input>', {type: 'text', name: 'startTime', placeholder: 'Please choose a Start Time', id: 'start', class: 'times'}))
+    $('#inputForm').append($('<input>', {type: 'text', name: 'endTime', placeholder: 'Please choose an End Time', id: 'end', class: 'times'}))
+  }
+
+  function createPeopleForm() {
+    $('#inputForm').append($('<input>', {type: 'text', name: 'invitee', placeholder: 'Choose your Guests', class: 'invitee', id: 'invitee'}))
+  }
+
+  function createDescriptionForm() {
+    $('#inputForm').append($('<input>', {type: 'text', name: 'invitee', placeholder: 'Create your Event Title', class: 'desc', id: 'desc'}))
+    $('#inputForm').append($('<input>', {type: 'text', name: 'desc', placeholder: 'Create your Event Description', class: 'desc', id: 'desc'}))
+    $('#inputForm').append($('<button>', {name: 'button', class: 'submitButton'}).text('ADD'))
+  }
+
+  $(document).on('click', '#invitesToggle', function() {
+    $('.times').hide()
+    $('.invitee').show()
+    $('.desc').hide()
+  })
+
+  $(document).on('click', '#timesToggle', function() {
+    $('.times').show()
+    $('.invitee').hide()
+    $('.desc').hide()
+  })
+
+  $(document).on('click', '#descriptionToggle', function() {
+    $('.times').hide()
+    $('.invitee').hide()
+    $('.desc').show()
+  })
 });
